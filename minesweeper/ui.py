c50bb9200be419759188832557058441d2916bf9
Monochrome <monochromaticsilver@gmail.com>
2024-11-13 11:54:13 -0800

    mines = randPoint

--- minesweeper/ui.py
+++ minesweeper/ui.py
@@ -3,7 +3,7 @@ import os
 
 
 def print_board(game_state: game_state):
-    # print(f" TODO print board (mines: {game_state.mines})")
+    print(f" TODO print board (mines: {game_state.mines})")
     # Task: Implement the method that prints the board
     pass
 
